fe0d92e40ab3913908fbcbfe7600746738345085
Tyler Jost <tyler_jost@utexas.edu>
2022-07-18 16:48:40 -0500

    split composite and attempt to stitch

--- scripts/cellMorphHelper.py
+++ scripts/cellMorphHelper.py
@@ -5,6 +5,36 @@ import cv2
 import numpy as np
 import pandas as pd
 import pickle
+
+import torch
+import detectron2
+
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import pandas as pd
+import os, json, cv2, random
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# Import image processing
+from skimage import measure
+from skimage import img_as_float
+from scipy.spatial import ConvexHull
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.structures import BoxMode
+
 # %%
 def makeNewExperimentDirectory(experimentName):
     """
@@ -33,7 +63,7 @@ def makeNewExperimentDirectory(experimentName):
     for folder in newFolders:
         os.makedirs(folder, exist_ok=True)
 
-def imSplit(im, nIms=16):
+def imSplit(im, nIms: int=16):
     """
     Splits images into given number of tiles
     Inputs:
@@ -104,6 +134,20 @@ def splitExpIms(experiment, nIms=16):
             newImPath = os.path.join(splitDir, 'phaseContrast', newImName)
             cv2.imwrite(newImPath, im)
 
+    print('Splitting composite')
+    # Split up composite images
+    imNames = os.listdir(os.path.join(dataDir, 'composite'))
+
+    for imName in imNames:
+        # Read and split mask
+        im = cv2.imread(os.path.join(dataDir, 'composite', imName))
+        tiles = imSplit(im, nIms)
+
+        # For each mask append a number, then save it
+        for num, im in enumerate(tiles):
+            newImName =  '.'.join([imName.split('.')[0]+'_'+str(num+1), imName.split('.')[1]])
+            newImPath = os.path.join(splitDir, 'composite', newImName)
+            cv2.imwrite(newImPath, im)   
     # Copy over labels
     originalTrain = os.path.join(dataDir, 'label', 'train')
     originalVal = os.path.join(dataDir,'label', 'val')
@@ -144,4 +188,34 @@ def convertLabels(labelDir):
                     labels[imBase][maskLabel] = group
     saveName = os.path.join(labelDir, 'labels.pkl')
     pickle.dump(labels, open(saveName, "wb"))
+
+def getSegmentModel(modelPath: str):
+    """
+    Gets a segmentation model that can be used to output masks
+    Inputs:
+    modelPath: Folder with model. Final model must be named model_final.pth
+    Outputs:
+    Mask-RCNN model
+    """
+    cfg = get_cfg()
+    if not torch.cuda.is_available():
+        print('CUDA not available, resorting to CPU')
+        cfg.MODEL.DEVICE='cpu'
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.DATASETS.TRAIN = ("cellMorph_Train",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+    cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 10000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.STEPS = []        # do not decay learning rate
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+    cfg.OUTPUT_DIR = modelPath
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+    predictor = DefaultPredictor(cfg)
+
+    return predictor
 # %%
---
+++ scripts/imgStitch.py
@@ -0,0 +1,79 @@
+# %% [markdown]
+"""
+# Stitching masks back together
+"""
+# %%
+import os
+import cv2
+import matplotlib.pyplot as plt
+from cellMorphHelper import getSegmentModel
+# %%
+def newGrid(n: int) -> list:
+    """
+    Makes grid outputs align with matplotlib subplots. IE grid numbering for a 3x3 split looks like:
+
+    1, 4, 7
+    2, 5, 8
+    3, 6, 9
+
+    However subplots do not go in this order. This outputs a list that allows for loading of the correct images
+    """
+    newGrid = []
+    for i in range(1, n+1):
+        gridNum = i
+        for j in range(1, n+1):
+            newGrid.append(gridNum)
+            gridNum+=n
+    return newGrid
+# %%
+predictor = getSegmentModel('../output/AG2021Split16')
+# %%
+
+# Get proper grid
+imPath = '../data/AG2021Split16/phaseContrast'
+testIm = 'phaseContrast_C5_1_2020y06m19d_00h33m'
+
+fullImage = cv2.imread(os.path.join('../data/AG2021/phaseContrast', testIm+'.jpg'))
+
+imNum = 1
+plt.figure(figsize=(5,5))
+imCt = 1
+for imNum in newGrid(4):
+    imPathFull = os.path.join(imPath, testIm+'_'+str(imNum)+'.jpg')
+
+    im = cv2.imread(imPathFull)
+
+    outputs = predictor(im)
+
+    v = Visualizer(im[:, :, ::-1],
+                    metadata=cell_metadata, 
+                    scale=1, 
+                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    plt.subplot(4,4,imCt)
+    plt.imshow(out.get_image()[:, :, ::-1])
+    plt.axis('off')
+    plt.title(imNum)
+    imCt += 1
+plt.show()
+# %%
+imNum = 9
+imPathFull = os.path.join(imPath, testIm+'_'+str(imNum)+'.jpg')
+
+im = cv2.imread(imPathFull)
+
+outputs = predictor(im)['instances']
+
+nCells = len(outputs)
+
+if nCells>0:
+    allMasks = np.zeros(outputs.image_size)
+    for nCell in range(nCells):
+        mask = outputs[nCell].pred_masks.numpy()[0]
+        mask = clear_border(mask)
+        allMasks = allMasks+mask
+# %%
+
+
+print(newGrid)
