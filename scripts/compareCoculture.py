c030c3e2bd049d1ce857d089cfc623a29f4306b1
Tyler Jost <tyler_jost@utexas.edu>
2022-08-12 11:31:08 -0500

    Separation of co/monoculture

--- scripts/compareCoculture.py
+++ scripts/compareCoculture.py
@@ -1,7 +1,9 @@
 # %%
 import pickle
-import skimage
 import random
+import pandas as pd
+from cellMorphHelper import procrustes
+import numpy as np
 # %%
 esamNeg=pickle.load(open('../results/TJ2201Split16ESAMNeg.pickle',"rb"))
 coculture=pickle.load(open('../results/TJ2201Split16CellPerims.pickle',"rb"))
@@ -12,3 +14,21 @@ random.seed(1234)
 coculturePerm = random.sample(range(len(coculture)), len(coculture))
 esamNegPerm = random.sample(range(len(esamNeg)), len(esamNeg))
 
+nDesired = 2000
+
+cocultureSub = [coculture[x] for x in coculturePerm if coculture[x].color=='red']
+esamNegSub = [esamNeg[x] for x in esamNegPerm if esamNeg[x].color=='red']
+
+cocultureSub = cocultureSub[0:nDesired]
+esamNegSub = esamNegSub[0:nDesired]
+
+# %% Align perimeters to each other
+referencePerim = esamNegSub[0].perimAligned
+
+for cell in cocultureSub:
+
+
+# %% Build dataframe
+labels = ['coculture' for x in range(len(cocoltureSub))]+['monoculture' for x in range(len(esamNeg))]
+
+coculturePerims = [cell.p]
