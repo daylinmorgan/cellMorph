cdc5248727bf7f6dfbda0dc04fe2fbe7ed5631e1
Tyler Jost <tyler_jost@utexas.edu>
2022-09-09 16:09:59 -0500

    changed cellpose data

--- scripts/readCellPoseNpy.py
+++ scripts/readCellPoseNpy.py
@@ -5,7 +5,8 @@ import os
 import matplotlib.pyplot as plt
 import pickle
 # %%
-maskDir = '../data/TJ2201/masks'
+maskDir = '../data/TJ2201/cellPoseOutputs'
+saveFolder = '../data/TJ2201/masks'
 maskFiles = [os.path.join(maskDir, file) for file in os.listdir(maskDir)]
 mask = np.load(maskFiles[0], allow_pickle=True)
 mask = mask.item()
@@ -13,7 +14,8 @@ imNum = 1
 nIms = len(maskFiles)
 for maskFile in maskFiles:
     print(f'{imNum}/{nIms}')
-    fname = maskFile.split('.npy')[0]+'.tif'.replace('composite', 'mask')
+    fname = maskFile.split('.npy')[0]+'.tif'
+    fname = fname.replace('composite','mask').replace('_seg','').replace(maskDir, saveFolder)
     mask = np.load(maskFile, allow_pickle=True).item()
     imsave(fname, mask['masks'])
     imNum += 1
